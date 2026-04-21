a12df37407d15de5ec986eb6cadc026824d9c24e
Steven Emad <stevenemad180@gmail.com>
2026-04-21 03:43:13 +0200

    fix session provider props typing

--- src/app/_Components/sessionprovider/SessionProviderwell.tsx
+++ src/app/_Components/sessionprovider/SessionProviderwell.tsx
@@ -1,11 +1,12 @@
 'use client'
+
 import { SessionProvider } from 'next-auth/react'
-import React from 'react'
-export default function SessionProviderwell({children}) {
-    return (
-        <SessionProvider>
-            
-            { children }
-  </SessionProvider>
-)
+import React, { ReactNode } from 'react'
+
+interface SessionProviderwellProps {
+  children: ReactNode
+}
+
+export default function SessionProviderwell({ children }: SessionProviderwellProps) {
+  return <SessionProvider>{children}</SessionProvider>
 }
